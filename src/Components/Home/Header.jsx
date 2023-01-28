5ef9d556eeb83f58ea16c3e5a1578fe944c1a3d4
jm4293 <107846495+jm4293@users.noreply.github.com>
2023-01-28 22:55:10 +0900

    feat: 헤더

---
+++ src/Components/Home/Header.jsx
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from "react";
+import styled from "styled-components";
+import {BiWifi} from "react-icons/bi"
+import {BsBatteryFull} from "react-icons/bs"
+
+function Header() {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const id = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+        return (() => clearInterval(id))
+    }, []);
+
+    return (
+        <Frame>
+            <Service>No Service <BiWifi/></Service>
+            <Time>{`${String(time.getHours()).padStart(2, "0")}:${String(time.getMinutes()).padStart(2, "0")}`}</Time>
+            <Battery>100% <BsBatteryFull/></Battery>
+        </Frame>
+    )
+}
+
+const Frame = styled.div`
+  width: 100%;
+  height: 4%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
+
+const Box = styled.div`
+  font-size: 26px;
+  margin: auto 0;
+`;
+
+const Service = styled(Box)`
+  text-align: left;
+`;
+
+const Time = styled(Box)`
+  text-align: center;
+`;
+
+const Battery = styled(Box)`
+  text-align: right;
+`;
+
+export default Header;
