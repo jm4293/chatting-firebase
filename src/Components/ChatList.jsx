6dad28d6ad91035f0bbec6c0b6c68898a3eb51e1
jm4293 <107846495+jm4293@users.noreply.github.com>
2023-01-31 11:56:52 +0900

    feat: 채팅 리스트 화면에 로그인된 아이디로 수신된 메시지 목록 화면에 출력

--- src/Components/ChatList.jsx
+++ src/Components/ChatList.jsx
@@ -1,61 +1,108 @@
 import React, {useEffect, useState} from "react";
 import styled from "styled-components";
-import {doc, getDoc} from "firebase/firestore";
+import {collection, doc, getDoc, getDocs} from "firebase/firestore";
 import {db, storage, messaging} from "../Firebase/firebase";
 import {getDownloadURL, ref} from "firebase/storage";
+import {useNavigate} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {RxMagnifyingGlass} from "react-icons/rx";
+import {BsPlusCircle} from "react-icons/bs";
+import {HiOutlineMusicNote} from "react-icons/hi";
+import {AiOutlineSetting} from "react-icons/ai";
 
 function ChatList() {
-    const [name, setName] = useState("");
-    const [photo, setPhoto] = useState("");
+    // const [name, setName] = useState("");
+    const [list, setList] = useState([]);
+    const navigate = useNavigate()
+
+    const name = useSelector((state) => {
+        return state.name.name;
+    })
 
     useEffect(() => {
         const uid = sessionStorage.getItem("uid");
 
         async function getUser() {
-            await getDoc(doc(db, "users", uid))
-                .then((result) => {
-                    setName(result.data().name)
-                })
-                .catch((error) => {
-                    console.log("ChatList getUser error ", error)
-                })
+            return await getDoc(doc(db, "users", uid))
         }
 
-        getUser();
+        getUser()
+            .then((result) => {
+                // console.log(result)
+                // setName(result.data().name)
+                // setNumber(result.data().number)
+            })
+            .catch((error) => {
+                console.log("Profile getUser error ", error)
+            })
 
-        async function getPhoto() {
-            await getDownloadURL(ref(storage, uid))
-                .then((result) => {
-                    setPhoto(result);
-                })
-                .catch((error) => {
-                    console.log("ChatList getPhoto error ", error)
-                })
+        async function getChatList() {
+            return await getDocs(collection(db, `chatList-${sessionStorage.getItem("uid")}`))
         }
 
-        getPhoto();
-        
+        getChatList()
+            .then((result) => {
+                result.forEach((doc) => {
+                    setList(list => [...list, doc.id]);
+                })
+            })
+            .catch((error) => {
+                console.log("ChatList getChatList error ", error)
+            })
     }, [])
 
     return (
         <Frame>
-            <h1 style={{borderBottom: "1px solid rgb(222, 222, 222)"}}>Chatting</h1>
-            <div>{name}</div>
-            {/*<img src={photo}/>*/}
+            <Header>
+                <div style={{display: "flex", alignItems: "center"}}>
+                    <h1>Chats</h1>
+                    <div style={{flexGrow: "1"}}></div>
+                    <RxMagnifyingGlass style={{transform: "scale(1.5)", marginRight: "40px"}}/>
+                    <BsPlusCircle style={{transform: "scale(1.5)", marginRight: "40px"}}/>
+                    <HiOutlineMusicNote style={{transform: "scale(1.5)", marginRight: "40px"}}/>
+                    <AiOutlineSetting style={{transform: "scale(1.5)"}}/>
+                </div>
+                <div>{`접속한 아이디: ${name} (${sessionStorage.getItem("uid")})`}</div>
+            </Header>
+            <ListWrap>
+                {
+                    list.map((element) => {
+                        return (
+                            <List onClick={() => navigate(`/chatting/${element}`)}>
+                                {
+                                    <div>
+                                        <div>{element}</div>
+                                    </div>
+                                }
+                            </List>
+                        )
+                    })
+                }
+            </ListWrap>
         </Frame>
     )
 }
 
 const Frame = styled.div`
-  width: 100%;
-  height: 100%;
+  width: 98%;
+  height: 95%;
 `;
 
-const Photo = styled.img`
-  width: 10%;
-  height: 10%;
-  margin: 0 20px;
-  border-radius: 30px;
+const Header = styled.div`
+  padding-bottom: 10px;
+  border-bottom: 1px solid rgb(200, 200, 200);
+`;
+
+const ListWrap = styled.div`
+  margin: auto;
+  width: 90%;
+`;
+
+const List = styled.div`
+  margin: 20px 0;
+  padding: 20px;
+  border-bottom: 1px solid rgb(222, 222, 222);
+  cursor: pointer;
 `;
 
 export default ChatList
